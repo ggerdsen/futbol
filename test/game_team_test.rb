107aab4b4d5d5738fe15fa6066da4ae5e3d200aa
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-06-03 15:39:19 -0600

    upodate game team test.

--- test/game_team_test.rb
+++ test/game_team_test.rb
@@ -13,4 +13,25 @@ class GameTeamTest < MiniTest::Test
     assert_equal 2012030221, game_team.game_id
   end
 
+  def test_it_can_make_hash
+    game_team = GameTeam.new({:game_id => 2012030221, :team_id => 3, :HoA => "away", :result => "LOSS", :settled_in => "OT", :head_coach => "John Tortorella", :goals => 2, :shots => 8, :tackles => 44, :pim => 8, :powerPlayOpportunities => 3, :powerPlayGoals => 0, :faceOffWinPercentage => 44.8, :giveaways => 17, :takeaways => 7})
+    expected = {
+                  :game_id=>2012030221,
+                  :face_off_win_percentage=>nil,
+                  :give_aways=>17,
+                  :goals=>2,
+                  :head_coach=>"John Tortorella",
+                  :hoa=>nil,
+                  :pim=>8,
+                  :power_play_goals=>nil,
+                  :power_play_opportunities=>nil,
+                  :result=>"LOSS",
+                  :settled_in=>"OT",
+                  :shots=>8,
+                  :tackles=>44,
+                  :take_aways=>7,
+                  :team_id=>3
+                }
+    assert_equal expected, game_team.to_hash
+  end
 end
